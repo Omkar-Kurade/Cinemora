37c8dbe954a33473145aa0639be2d0ce34042e35
Omkar-Kurade <omkarkurade14@gmail.com>
2026-02-01 13:39:47 +0530

    background code and stick search bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Search from "./components/search";
 import Spinner from "./components/spinner";
 import MovieCard from "./components/movieCard";
+import LightRays from "./components/lightRays";
 import { getTrendingMovies, updateSearchCount } from "./appwrite";
 
 
@@ -19,21 +20,24 @@ const API_OPTIONS = {
 };
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, SetMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [debounceSearchTerm , setDebounceSearchTerm] = useState()
-  const [trendingMovies , setTrendingMovies] = useState([])
 
-  useDebounce(()=>setDebounceSearchTerm(searchTerm) , 500 , [searchTerm])
+  const [debounceSearchTerm, setDebounceSearchTerm] = useState()
+
+  const [trendingMovies, setTrendingMovies] = useState([])
+
+  useDebounce(() => setDebounceSearchTerm(searchTerm), 500, [searchTerm])
 
   const fetchMovies = async (query = "") => {
     setIsLoading(true);
     setErrorMessage("");
     try {
-      const endpoint = query 
-      ?`${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` 
-      :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       if (!response.ok) {
@@ -48,42 +52,58 @@ function App() {
       }
       SetMovieList(data.results || []);
 
-      if(query && data.results.length > 0){ 
-        await updateSearchCount(query , data.results[0])
-   
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0])
+
 
       }
 
     } catch (error) {
       console.error(`Error in fetching Movies : ${error}`);
       setErrorMessage("Error fetching movies.Please try again later.");
-    }finally{
+    } finally {
       setIsLoading(false)
     }
   };
 
-  const loadTrendingMovies = async ()=>{
+  const loadTrendingMovies = async () => {
     try {
-      const movies = await getTrendingMovies()    
-       setTrendingMovies(movies)
+      const movies = await getTrendingMovies()
+      setTrendingMovies(movies)
     } catch (error) {
       console.error(`Error fetching trending Movies : ${error}`)
-   
+
     }
   }
 
   useEffect(() => {
-    
+
     fetchMovies(debounceSearchTerm);
   }, [debounceSearchTerm]);
 
-  useEffect(()=>{
+  useEffect(() => {
     loadTrendingMovies()
-  } ,[])
+  }, [])
 
   return (
-    <main>
-      <div className="pattern" />
+    <main className="relative min-h-screen bg-black"  >
+      <div className="absolute inset-0 z-0 pointer-events-none">
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#fc3b14"
+          raysSpeed={2}
+          lightSpread={0.5}
+          rayLength={3}
+          followMouse={true}
+          mouseInfluence={0}
+          noiseAmount={0}
+          distortion={2}
+          className="custom-rays"
+          pulsating={false}
+          fadeDistance={0.5}
+          saturation={0.5}
+        />
+      </div>
       <div className="wrapper">
         <header>
           <img src="./hero.png" alt="" />
@@ -91,16 +111,16 @@ function App() {
             Discover <span className="text-gradient">Movies</span> You’ll Love —
             Effortlessly
           </h1>
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        {trendingMovies.length > 0  &&  (
-          <section className="trending">
-            <h2>Trending Movies</h2>
+         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        {trendingMovies.length > 0 && (
+          <section className="trending font-luckiest-guy">
+            <h2 >Trending Movies</h2>
             <ul>
-              {trendingMovies.map((movie , index)=>(
+              {trendingMovies.map((movie, index) => (
                 <li key={movie.$id}>
                   <p>{index + 1}</p>
-                   <img src={movie.poster_url} alt={movie.title} />
+                  <img src={movie.poster_url} alt={movie.title} />
                 </li>
               ))}
             </ul>
@@ -110,19 +130,43 @@ function App() {
           <h2 className="font-luckiest-guy ">All Movies</h2>
 
           {isLoading ? (
-            <Spinner/>
+            <Spinner />
           ) : errorMessage ? (
-             <p className="text-red-500">{errorMessage}</p>
+            <p className="text-red-500">{errorMessage}</p>
           ) : (
             <ul>
-              {movieList.map((movie)=>(
-               <MovieCard key={movie.id} movie={movie}/>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
         </section>
       </div>
+
+
+      <footer className="bg-neutral-primary-soft rounded-base shadow-xs border border-default m-4 font-luckiest-guy">
+        <div className="w-full max-w-7xl mx-auto p-4 md:py-8">
+          <div className="sm:flex sm:items-center sm:justify-between ">
+            <a href="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse text-gray-600">
+              <img src="./logo.png" class="h-7" />
+              <span class="text-heading self-center text-2xl font-semibold whitespace-nowrap">Cinemora</span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-body sm:mb-0  text-gray-600">
+              <li>
+                <a href="https://github.com/Omkar-Kurade/Cinemora" target="_blank" className="hover:underline me-4 md:me-6">Github</a>
+              </li>
+              <li>
+                <a href="https://linkedin.com/in/omkar-kurade-054668223" target="_blank" className="hover:underline">LinkedIn</a>
+              </li>
+            </ul>
+          </div>
+          <hr className="my-6 border-defaul border-gray-500 t sm:mx-auto lg:my-8" />
+          <span className="block text-sm text-body sm:text-center text-gray-600">© 2025</span>
+        </div>
+      </footer>
+
     </main>
+
   );
 }
 
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,6 +1,6 @@
 const Search = ({ searchTerm, setSearchTerm }) => {
   return (
-    <div className="search">
+    <div className="search sticky top-0 z-50 w-full py-4 backdrop-blur-md bg-black/20h">
       <div>
         <img src="search.svg" alt="search" />
         <input
